d8d9c21a31a4d913582c6d1ae94d55f3f50a3691
mktcompracerta-design <mktcompracerta@gmail.com>
2025-11-29 01:56:14 -0300

    Update gerar.js

--- api/gerar.js
+++ api/gerar.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
       const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash-latest"
+        model: "gemini-1.5-flash"
       });
 
       const inputs = [];
